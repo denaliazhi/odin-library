dba1045627b90fa147ff810073d0bbd6e06a0a08
Denalia Zhi <d.naliazhi@gmail.com>
2025-07-08 16:01:53 -0400

    Write function to update shelf display (detailed, matches sample book cover)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 PREMISE
 
-This app will allow users to maintain a digital "library" 
+This app will allow users to maintain a digital 'library' 
 of books. Users will be able to add/remove records to/from 
 the library. Each record of a book will show its title, 
 author, page count, and read status (whether the user has
@@ -12,9 +12,6 @@ read the book).
 
 // PSEUDOCODE
 // Javascript:
-
-const shelf = document.querySelector(".container");
-console.log(shelf);
 
 /*
 TO STORE BOOKS
@@ -32,7 +29,7 @@ Constructor: Book()
 */
 function Book(title, author, pages, read) {
     if (!new.target) {
-        throw Error("Expecting 'new' operator when calling constructor")
+        throw Error('Expecting "new" operator when calling constructor')
     }
     this.id = crypto.randomUUID();
     this.title = title;
@@ -62,30 +59,85 @@ TO UPDATE SHELF DISPLAY
 Function: updateDisplay()
     1. Loop through array
     2. For each book in library, add its properties as
-    text to HTML card (title, author, pages, read)
+    text to HTML card (title, author, pages)
     3. Assign id property to data-id attribute of card
     4. Determine icon (thumbs up / down) for read status
     5. Generate random number to determine color of
-    book cover and assign bg-image of card
+    book cover and assign bg-image of cover
     6. Assign matching style classes to card
     7. Append card to shelf container
 */
 
 function updateDisplay() {
     for (const book of library) {
-        const bookCover = document.createElement("p");
-        console.log(book);
-        bookCover.textContent = `${book.title}
-                                 ${book.author}
-                                 ${book.pages}
-                                 ${book.read}`;
-        shelf.appendChild(bookCover);
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('data-id', book.id);
+
+        const readStatus = document.createElement('p');
+        readStatus.classList.add('read-status');
+        readStatus.textContent = book.read ? 'didst read' : 'didst not read';
+        
+        card.appendChild(readStatus);
+
+        // -- Side bar components --
+
+        const sidebar = document.createElement('div');
+        sidebar.classList.add('side-bar');
+
+        const toggleRead = document.createElement('div');
+
+        const thumbUp = document.createElement('img');
+        thumbUp.src = 'assets/thumb-up.png';
+        thumbUp.alt = 'Thumbs up icon';
+
+        const thumbDown = document.createElement('img');
+        thumbDown.src ='assets/thumb-down.png';
+        thumbDown.alt = 'Thumbs down icon';
+
+        if (book.read) {
+            // "not read" icon gets lower opacity
+            thumbDown.classList.add('inactive'); 
+        } else {
+            thumbUp.classList.add('inactive');
+        }
+
+        const remove = document.createElement('button');
+        remove.textContent = 'x';
+
+        toggleRead.appendChild(thumbUp);
+        toggleRead.appendChild(thumbDown);
+        sidebar.appendChild(toggleRead);
+        sidebar.appendChild(remove);
+        card.appendChild(sidebar);
+
+        // -- Book cover components --
+        const cover = document.createElement('div');
+        cover.classList.add('cover');
+
+        const title = document.createElement('h2')
+        title.textContent = book.title;
+
+        const author = document.createElement('p')
+        author.textContent = book.author;
+
+        const pages = document.createElement('p')
+        pages.textContent = book.pages;
+
+        cover.appendChild(title);
+        cover.appendChild(author);
+        cover.appendChild(pages);
+        card.appendChild(cover);
+
+        // Add book to shelf
+        const shelf = document.querySelector('.container');
+        shelf.appendChild(card);
     }
 }
 
-
-addBook("Hello", "Goodbye", "123", true);
-addBook("Howdy", "Partner", "246", false);
+addBook('Hello', 'Goodbye', '123', true);
+addBook('Howdy', 'Partner', '246', false);
 
 updateDisplay();
 
